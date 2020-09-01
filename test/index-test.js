ff247a9d2f7da41d41570a0c7d0df77bef3a5496
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-09-01 23:43:35 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,4 @@
- function theBeatlesPlay(musician,instruments) {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+ 
 
     function theBeatlesPlay(musicians, instruments) {
       var array = [];
