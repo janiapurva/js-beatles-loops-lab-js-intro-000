e8f29c50e0a876f6c61401015aba1506b50b7f1b
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-09-01 23:09:07 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
-function theBeatlesPlay(musician,instruments) {
-   musicians : ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-   instruments : ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+ function theBeatlesPlay(musician,instruments) {
+    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 }
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
@@ -14,7 +14,7 @@ function theBeatlesPlay(musician,instruments) {
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
     });
-
+    
     it("takes in any array of strings and returns a new array with exclamation points added to each element", function() {
       expect(johnLennonFacts([
         "While on tour, he enjoyed playing Monopoly",
