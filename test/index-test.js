88d3d1d577044ad4acaee1129c20055de27b3cf3
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-09-01 23:43:04 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
  function theBeatlesPlay(musician,instruments) {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-    
+
     function theBeatlesPlay(musicians, instruments) {
       var array = [];
       for (var i = 0; i < musicians.length; i++) {
@@ -9,7 +9,7 @@
       }
       return array;
     }
-    
+
     function johnLennonFacts() {
       var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
       var array = [];
@@ -20,7 +20,7 @@
       }
       return array;
     }
-    
+
     function iLoveTheBeatles(number) {
       var array = [];
       do {
